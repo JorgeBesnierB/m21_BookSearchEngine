ecfc38c40409796e4fadfa108951a585c8747d88
Jorge Besnier <jorgebesnierb@outlook.com>
2022-11-24 16:40:26 -0600

    server code files to add apolo server/graphql

--- server/server.js
+++ server/server.js
@@ -3,8 +3,27 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+//JBE:Apollo Server
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//JBE:Graph QL
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +33,12 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
+
+// JBE: 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
